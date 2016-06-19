86457274ec5adcfa12963f6e68105d0cfb3140b5
Chris Boette <cboette@gmail.com>
2016-06-19 01:16:33 -0500

    Style change.

--- app/components/Profile.js
+++ app/components/Profile.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router';
 
-import CatTile from './CatTile.js';
+import CatTile from './CatTile';
 
 export const CatProfiles = ({ cats }) => (
   <ul>
