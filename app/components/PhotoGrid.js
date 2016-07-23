7cb0050ba54354e6d1d356df3bbe2095b7ef416a
Chris Boette <cboette@gmail.com>
2016-07-23 14:23:07 -0500

    Add working code to PhotoGrid component. Add test for said component.

--- app/components/PhotoGrid.js
+++ app/components/PhotoGrid.js
@@ -1,5 +1,19 @@
 import React from 'react';
+import _ from 'lodash';
 
-const PhotoGrid = () => ('');
+const PhotoGrid = ({ photos }) => (
+  <div className="photoGrid">
+    <ul>
+      {_.map(photos, (photo, key) => (
+        <li key={key}><img src={photo} alt="It is a cat." /></li>
+      ))}
+    </ul>
+  </div>
+);
+
+
+PhotoGrid.propTypes = {
+  photos: React.PropTypes.object.isRequired
+};
 
 export default PhotoGrid;
