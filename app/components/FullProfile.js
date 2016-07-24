142e9de5834a49420665908f520036e25ca3a82c
Chris Boette <cboette@gmail.com>
2016-07-24 15:05:36 -0500

    Remove wishlist for now. Validate cat proptypes.

--- app/components/FullProfile.js
+++ app/components/FullProfile.js
@@ -2,9 +2,8 @@ import React from 'react';
 import { connect } from 'react-redux';
 
 import PhotoGrid from './PhotoGrid';
-import Wishlist from './Wishlist';
 
-export const FullProfileContainer = ({ user, cat }) => (
+export const FullProfileContainer = ({ cat }) => (
   <div id="fullProfile">
     <div id="mainImage">
       <img src={cat.avatar} alt={`It's ${cat.name}!`} />
@@ -21,18 +20,22 @@ export const FullProfileContainer = ({ user, cat }) => (
     <div id="profile">
       {cat.about}
     </div>
-    <Wishlist uid={user.uid} id={cat.id} />
   </div>
 );
 
 FullProfileContainer.propTypes = {
-  user: React.PropTypes.shape({
-    uid: React.PropTypes.string.isRequired
-  }).isRequired,
-  cat: React.PropTypes.object.isRequired
+  cat: React.PropTypes.shape({
+    about: React.PropTypes.string.isRequired,
+    age: React.PropTypes.number.isRequired,
+    avatar: React.PropTypes.string.isRequired,
+    color: React.PropTypes.string.isRequired,
+    name: React.PropTypes.string.isRequired,
+    public: React.PropTypes.object.isRequired,
+    sex: React.PropTypes.string.isRequired
+  }).isRequired
 };
 
-const mapStateToFullProfileContainerProps = (state) => ({ user: state.user, cats: state.cats });
+const mapStateToFullProfileContainerProps = (state) => ({ cats: state.cats });
 const FullProfile = connect(mapStateToFullProfileContainerProps)(FullProfileContainer);
 
 export default FullProfile;
