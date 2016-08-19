7a1df84e5f43a609ffd4094117534e2d4b8d3ccc
Chris Boette <cboette@gmail.com>
2016-08-19 17:47:13 -0500

    Fill out Wishlist component amd test.

--- app/components/Wishlist.js
+++ app/components/Wishlist.js
@@ -1,5 +1,34 @@
+import _ from 'lodash';
 import React from 'react';
 
-const Wishlist = () => ('');
+const Wishlist = ({ wishlist }) => (
+  <div className="wishlist">
+    <ul>
+      {_.map(wishlist.toys, (toy, key) => (
+        <li key={key}><a href={toy.link}>{toy.name}</a></li>
+      ))}
+    </ul>
+
+    <ul>
+      {_.map(wishlist.food, (food, key) => (
+        <li key={key}><a href={food.link}>{food.name}</a></li>
+      ))}
+    </ul>
+
+    <ul>
+      {_.map(wishlist.litter, (litter, key) => (
+        <li key={key}><a href={litter.link}>{litter.name}</a></li>
+      ))}
+    </ul>
+  </div>
+);
+
+Wishlist.propTypes = {
+  wishlist: React.PropTypes.shape({
+    toys: React.PropTypes.object.isRequired,
+    food: React.PropTypes.object.isRequired,
+    litter: React.PropTypes.object.isRequired,
+  }).isRequired
+};
 
 export default Wishlist;
--- test/components/Wishlist_spec.js
+++ test/components/Wishlist_spec.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import expect from 'expect';
 import React from 'react';
 import TestUtils from 'react-addons-test-utils';
@@ -6,7 +7,7 @@ import { catProfile } from '../test_helper';
 
 function wishlistSetup() {
   const props = {
-    cat: catProfile
+    wishlist: catProfile.wishlist
   };
 
   const renderer = TestUtils.createRenderer();
@@ -19,7 +20,12 @@ function wishlistSetup() {
 describe('Component: Wishlist', () => {
   const output = wishlistSetup();
 
-  it('is true', () => {
-    expect(true).toBe(true);
+  it('renders three unordered lists in a div', () => {
+    expect(output.type).toBe('div');
+    expect(output.props.className).toBe('wishlist');
+    expect(output.props.children.length).toBe(3);
+
+    const allUl = _.every(output.props.children, (kid) => kid.type === 'ul');
+    expect(allUl).toBe(true);
   });
 });
