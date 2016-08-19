acbe21f26891ab6b0a2384c9d275145d40842f8e
Chris Boette <cboette@gmail.com>
2016-08-19 18:51:49 -0500

    Update AddProfile component name.

--- app/config/routes.js
+++ app/config/routes.js
@@ -2,14 +2,14 @@ import React from 'react';
 import Main from '../components/Main';
 import Home from '../components/Home';
 import Profile from '../components/Profile';
-import addProfile from '../components/addProfile';
+import AddProfile from '../components/AddProfile';
 import Upload from '../components/Upload';
 import { Route, IndexRoute } from 'react-router';
 
 export default (
   <Route path="/" component={Main}>
     <Route path="/profiles" component={Profile} />
-    <Route path="/profiles/add" component={addProfile} />
+    <Route path="/profiles/add" component={AddProfile} />
     <Route path="/upload" component={Upload} />
     <IndexRoute component={Home} /> {/* default route */}
   </Route>
