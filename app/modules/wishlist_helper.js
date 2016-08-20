b974dcd8b70677662ceb8c86f9f16ed65eb725b4
Chris Boette <cboette@gmail.com>
2016-08-19 19:44:58 -0500

    Add category to start of form value formatter.

--- app/modules/wishlist_helper.js
+++ app/modules/wishlist_helper.js
@@ -1,9 +1,10 @@
 import _ from 'lodash';
 import { AFFILIATE_CODE } from '../config/CONSTANTS';
 
-export function urlFormatter(input) {
-  const [rawName, rawUrl] = input.split('|');
+export function formValFormatter(input) {
+  const [rawCategory, rawName, rawUrl] = input.split('|');
+  const category = _.capitalize(rawCategory);
   const name = rawName.split('_').map(_.capitalize).join(' ');
   const url = `${rawUrl}${AFFILIATE_CODE}`;
-  return { name, url };
+  return { category, name, url };
 }
--- test/modules/wishlist_helper_spec.js
+++ test/modules/wishlist_helper_spec.js
@@ -1,11 +1,15 @@
 import expect from 'expect';
 
-import { urlFormatter } from '../../app/modules/wishlist_helper';
+import { formValFormatter } from '../../app/modules/wishlist_helper';
 import { AFFILIATE_CODE } from '../../app/config/CONSTANTS';
 
-const displayedVal = 'fictional_wet_food|https://amzn.co/poiuytr';
+const displayedVal = 'food|fictional_wet_food|https://amzn.co/poiuytr';
+const category = 'Food';
 const productName = 'Fictional Wet Food';
 const productUrl = 'https://amzn.co/poiuytr';
 
-expect(urlFormatter(displayedVal))
-  .toEqual({ name: productName, url: `${productUrl}${AFFILIATE_CODE}` });
+expect(formValFormatter(displayedVal)).toEqual({
+  category,
+  name: productName,
+  url: `${productUrl}${AFFILIATE_CODE}`
+});
