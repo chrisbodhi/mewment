690475d206095c1985895c8b1137ccf0bcf1e189
Chris Boette <cboette@gmail.com>
2016-08-27 11:40:30 -0700

    Add Feed page to routes.

--- app/config/routes.js
+++ app/config/routes.js
@@ -1,14 +1,17 @@
 import React from 'react';
+import { Route, IndexRoute } from 'react-router';
+
 import Main from '../components/Main';
 import Home from '../components/Home';
 import Profile from '../components/Profile';
 import AddProfile from '../components/AddProfile';
 import FullProfile from '../components/FullProfile';
 import Upload from '../components/Upload';
-import { Route, IndexRoute } from 'react-router';
+import Feed from '../components/Feed';
 
 export default (
   <Route path="/" component={Main}>
+    <Route path="/feed" component={Feed} />
     <Route path="/profiles" component={Profile} />
     <Route path="/profiles/add" component={AddProfile} />
     <Route path="/profiles/:id" component={FullProfile} />
