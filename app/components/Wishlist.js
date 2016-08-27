40af0ccf2d9c95ca53db0122f749e97e78869e2a
Chris Boette <cboette@gmail.com>
2016-08-26 18:28:41 -0700

    Clean up wishlist code so it works.

--- app/components/Wishlist.js
+++ app/components/Wishlist.js
@@ -1,26 +1,16 @@
-import _ from 'lodash';
 import React from 'react';
 
 const Wishlist = ({ wishlist }) => (
-  <div className="wishlist">
-    <ul>
-      {_.map(wishlist.toys, (toy, key) => (
-        <li key={key}><a href={toy.link}>{toy.name}</a></li>
-      ))}
-    </ul>
-
-    <ul>
-      {_.map(wishlist.food, (food, key) => (
-        <li key={key}><a href={food.link}>{food.name}</a></li>
-      ))}
-    </ul>
-
-    <ul>
-      {_.map(wishlist.litter, (litter, key) => (
-        <li key={key}><a href={litter.link}>{litter.name}</a></li>
-      ))}
-    </ul>
-  </div>
+  wishlist ?
+    <div className="wishlist">
+      <h5>Wishlist</h5>
+      <ul>
+        <li key={Math.random()}><a href={wishlist.toys.url}>{wishlist.toys.name}</a></li>
+        <li key={Math.random()}><a href={wishlist.food.url}>{wishlist.food.name}</a></li>
+        <li key={Math.random()}><a href={wishlist.litter.url}>{wishlist.litter.name}</a></li>
+      </ul>
+    </div> :
+    <div>No wishlist -- sad cat face</div>
 );
 
 Wishlist.propTypes = {
