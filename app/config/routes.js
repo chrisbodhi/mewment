9ca2a253c4dbfb09d5e72650a76968a7ea35747d
Chris Boette <cboette@gmail.com>
2016-09-04 14:36:04 -0500

    Add Picker component to route for Matching.

--- app/config/routes.js
+++ app/config/routes.js
@@ -1,14 +1,21 @@
 import React from 'react';
+
 import Main from '../components/Main';
 import Home from '../components/Home';
+
+import Picker from '../components/Picker';
+
 import Profile from '../components/Profile';
 import AddProfile from '../components/AddProfile';
 import FullProfile from '../components/FullProfile';
+
 import Upload from '../components/Upload';
+
 import { Route, IndexRoute } from 'react-router';
 
 export default (
   <Route path="/" component={Main}>
+    <Route path="/matching" component={Picker} />
     <Route path="/profiles" component={Profile} />
     <Route path="/profiles/add" component={AddProfile} />
     <Route path="/profiles/:id" component={FullProfile} />
