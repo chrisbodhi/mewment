6e38701c7fce26cb19ed36cc7f496f697eef4d99
Chris Boette <cboette@gmail.com>
2016-09-04 16:42:22 -0500

    WIP on loading the Picker component with relevant, actionable data.

--- app/components/Picker.js
+++ app/components/Picker.js
@@ -10,33 +10,34 @@ import PhotoGrid from './PhotoGrid';
 // i.e. new user signup means that the new user's id gets added to an array associated with the uid
 // that's called something like "new cats that you maybe want to follow"
 
-export const PickerContainer = ({ following, uid, users }) => {
-  const uidsToFollow = _.omit(users, [..._.keys(following), uid]);
-  // Don't want a double `map` as a one-liner
-  // eslint-disable-next-line
-  const avatarsToFollow = _.map(uidsToFollow, (userObj) => {
+export const PickerContainer = ({ following, users }) => {
+  // this leaves out future cats added by a user that the logged-in user already follows
+  // todo: rectify that shit
+  const uidsToFollow = _.omit(users, ..._.keys(following));
 
+  const avatarsToFollow = _.reduce(uidsToFollow, (acc, userObj) => {
     // todo: make this return an object because that's what we need
     // possible derp solution: val is aCat.avatar, key is ownerId-catId
+    // [ { ownerId-catId: aCat.avatar } ]
     return _.map(userObj, aCat => aCat.avatar);
-  });
+  }, {});
   return (<div>
-    {/* `photos` wants an array of cat photos */}
-    {/* pass in array of avatars that user is not following already */}
-    {/* todo: derp -- how to pick and how to communicate that the catId and its owner's id? */}
+    {/* `photos` wants an object of cat photos */}
+    {/* pass in object of avatars that user is not following already */}
+    {/* todo: derp -- how to pick and communicate that the catId and its owner's id? see :22 */}
+    {/* todo: change PhotoGrid so that it accepts a clickHandler as an optional prop */}
+    {/* todo: ...then add a clickHandler that pushes clicked avatar's user id to `following` */}
     <PhotoGrid photos={avatarsToFollow} />
   </div>);
 };
 
 PickerContainer.propTypes = {
   following: React.PropTypes.object.isRequired,
-  uid: React.PropTypes.string.isRequired,
-  users: React.PropTypes.object.isRequired
+  users: React.PropTypes.array.isRequired
 };
 
 const mapStateToPickerContainerProps = (state) => ({
   following: state.following,
-  uid: state.user.uid,
   users: state.users
 });
 const Picker = connect(mapStateToPickerContainerProps)(PickerContainer);
